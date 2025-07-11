f8f42bad0ef1c03da1be99215b4b6da253a55914
Darshan <darshanbele4@gmail.com>
2025-07-11 10:09:43 +0530

    Day 11

---
+++ Day11/src/com/tnsif/day11/finallyblock/Nestedtry.java
@@ -0,0 +1,11 @@
+package com.tnsif.day11.finallyblock;
+
+public class Nestedtry {
+
+	public static void main(String[] args) {
+		
+		NestedtryCatch.check();
+
+	}
+
+}
---
+++ Day11/src/com/tnsif/day11/multiplecatchblock/MultipleCatch.java
@@ -0,0 +1,13 @@
+package com.tnsif.day11.multiplecatchblock;
+
+public class MultipleCatch {
+
+	public static void main(String[] args) {
+		
+		System.out.println("I am in main method ");
+		
+		Division.divide();
+
+	}
+
+}
